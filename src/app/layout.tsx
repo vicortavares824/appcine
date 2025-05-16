74f1b29b9b54adcb4aab4141a5822d2652ddf8bd
vicortavares824 <vitinhotavares824@gmail.com>
2025-05-16 18:02:54 -0300

    nav bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Navbar></Navbar>
+        <Navbar/>
         {children}
       </body>
     </html>
--- src/componets/navbar/index.tsx
+++ src/componets/navbar/index.tsx
@@ -1,22 +1,32 @@
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
-            <div className="container-fluid">
-                <a className="navbar-brand fw-bold fs-4" href="#">Filmes & Séries</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav ms-auto">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#filmes">Filmes</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#series">Séries</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
+  <div className="container-fluid">
+    <a className="navbar-brand fw-bold fs-4" href="#">Filmes & Séries</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavApp" aria-controls="navbarNavApp" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavApp">
+      <ul className="navbar-nav ms-auto">
+        
+        <li className="nav-item">
+          <a className="nav-link" href="#filmes">Filmes</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#series">Séries</a>
+        </li>
+        
+        <li className="nav-item ms-lg-3">
+          <a className=" btn btn-outline-dark rounded-pill me-2" href="#login">Login</a>
+        </li>
+        <li className="nav-item ms-lg-3">
+          <a className="btn btn-outline-success rounded-pill" href="#signup">Cadastrar</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  
+</nav>
+
     )
 }
